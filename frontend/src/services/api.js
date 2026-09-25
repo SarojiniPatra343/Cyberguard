3d949fba3e46fc680383a233ca8317373b7b9091
sarojinipatra343 <sarojinipatra343@gmail.com>
2026-09-25 21:34:54 +0530

    Connect frontend to deployed Django authentication

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -18,7 +18,7 @@ const API_URL =
 
 // LOCAL DEVELOPMENT
 const DJANGO_API_URL =
-    "http://127.0.0.1:8001/api";
+    "https://cyberguard-mmor.onrender.com/api";
 
 
 // ============================================================
